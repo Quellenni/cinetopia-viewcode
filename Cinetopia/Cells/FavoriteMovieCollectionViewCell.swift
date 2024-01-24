269cc9f4af3d5df6b40dabf9504fe2164865ad93
Quellenni Reis <quellenni.reis@Quellennis-MacBook-Air.local>
2024-01-23 21:29:13 -0300

    Implements favorites screen and button

---
+++ Cinetopia/Cells/FavoriteMovieCollectionViewCell.swift
@@ -0,0 +1,90 @@
+//
+//  FavoriteMovieViewController.swift
+//  Cinetopia
+//
+//  Created by Quellenni Reis on 23/01/24.
+//
+
+import UIKit
+
+class FavoriteMovieCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - UI Components
+    
+    private lazy var moviePosterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 12.0
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private lazy var movieTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 20.0, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        
+        return label
+        
+    }()
+    
+    private lazy var favoriteButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let iconImage = UIImage(systemName: "heart.fill")?.withTintColor(.buttonBackground, renderingMode: .alwaysOriginal)
+        button.setImage(iconImage, for: .normal)
+        button.addTarget(self, action: #selector(didTapFavoriteButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    //MARK: - View life cycle
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Class methods
+    
+    private func setupConstraints() {
+        addSubview(moviePosterImageView)
+        moviePosterImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        moviePosterImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        moviePosterImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        moviePosterImageView.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        
+       addSubview(movieTitleLabel)
+        movieTitleLabel.topAnchor.constraint(equalTo: moviePosterImageView.bottomAnchor, constant: 12).isActive = true
+        movieTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        movieTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        addSubview(favoriteButton)
+        favoriteButton.topAnchor.constraint(equalTo: movieTitleLabel.bottomAnchor, constant: 7).isActive = true
+        favoriteButton.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        favoriteButton.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        favoriteButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    }
+    
+    func setupView(_ movie: Movie) {
+        let url = URL(string: movie.image)
+        moviePosterImageView.kf.setImage(with: url)
+        
+        movieTitleLabel.text = movie.title
+    }
+    
+    //MARK: -IBAction
+    
+    @objc
+    func didTapFavoriteButton(_ sender: UIButton) {
+        print("did tap favorite button")
+    }
+}
---
+++ Cinetopia/ViewControllers/FavoriteMoviesViewController.swift
@@ -0,0 +1,76 @@
+//
+//  FavoriteMoviesViewController.swift
+//  Cinetopia
+//
+//  Created by Quellenni Reis on 23/01/24.
+//
+
+import UIKit
+
+class FavoriteMoviesViewController: UIViewController {
+    
+    //MARK: - UI Components
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 27, bottom: 10, right: 27)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(FavoriteMovieCollectionViewCell.self, forCellWithReuseIdentifier: "FavoriteMovieCollectionViewCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        return collectionView
+    }()
+    
+    //MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .background
+        setupConstraints()
+    }
+
+
+    //MARK: - Class methods
+
+    private func setupConstraints() {
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension FavoriteMoviesViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteMovieCollectionViewCell", for: indexPath) as? FavoriteMovieCollectionViewCell else {
+            fatalError("error to create FavooriteMovieCollectionViewCell")
+        }
+        
+        let currentMovie = movies[indexPath.item]
+        cell.setupView(currentMovie)
+        
+        return cell
+    }
+}
+
+extension FavoriteMoviesViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/3, height: 200)
+    }
+}
+
+#Preview {
+    FavoriteMoviesViewController()
+}
